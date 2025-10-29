47ad79ba9505e7ef103922e6bccc77ac593b9aa7
Amar Narayan Dwivedi <amardwivedi792@gmail.com>
2025-10-30 00:32:32 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,30 +3,10 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    sitemap({
-      hostname: "https://www.vedic-pooja.com",
-      routes: [
-        "/",
-        "/about",
-        "/astrology",
-        "/pooja",
-        "/blog",
-        "/blog/1",
-        "/blog/2",
-        "/blog/3",
-        "/pooja/open",
-      ],
-      changefreq: "weekly",
-      priority: 0.8,
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
